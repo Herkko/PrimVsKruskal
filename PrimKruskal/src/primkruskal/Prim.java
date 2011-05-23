522b83bf74334851a1332232b43f609e7137e3b7
Herkko <Herkko.virolainen@gmail.com>
2011-05-23 15:52:51 +0300

    uusia luokkia

---
+++ PrimKruskal/src/primkruskal/Prim.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package primkruskal;
+
+/**
+ *
+ * @author herkko
+ */
+public class Prim {
+int solmujenmaara;
+int kaarienmaara;
+
+    public void Prim(){
+
+    }
+}
