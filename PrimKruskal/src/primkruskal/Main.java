8d7ed4312e7bc7da3f7af1415ac76df982219741
Herkko <Herkko.virolainen@gmail.com>
2011-05-23 11:09:54 +0300

    Projektin luonti

---
+++ PrimKruskal/src/primkruskal/Main.java
@@ -0,0 +1,18 @@
+
+
+package primkruskal;
+
+/**
+ *
+ * @author herkko
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        
+    }
+
+}
